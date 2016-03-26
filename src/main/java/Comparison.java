5b41c1ec9e2621ec8e10fcec5e24c59484e66b8e
mikias ayalew <ammrdf@yahoo.com>
2016-03-26 15:49:34 -0700

    Comparison

---
+++ src/main/java/Comparison.java
@@ -0,0 +1,44 @@
+/**
+ *   File Name: Comparison.java<br>
+ *
+ *   LastName, FirstName<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor: Jean-francois Nepton<br>
+ *   Created: Mar 26, 2016
+ *
+ */
+
+/**
+ * Comparison //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ *
+ * @author LastName, FirstName
+ * @version 1.0.0
+ * @since 1.0
+ *
+ */
+public class Comparison {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		// Local variable
+		int num = 10;
+
+		// If statement
+		if (num >= 10 || num <= 10) {
+
+			System.out.println("number is greater than 10");
+		} else {
+
+			System.out.print("number is smaller then 10");
+
+		}
+	}
+
+}
