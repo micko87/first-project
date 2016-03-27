7cc34390808cb7000b35b88e25da8dfde465a3cb
mikias ayalew <ammrdf@yahoo.com>
2016-03-26 18:10:53 -0700

    Counting

---
+++ src/main/java/Counting.java
@@ -0,0 +1,35 @@
+/**
+ *   File Name: Counting.java<br>
+ *
+ *   LastName, FirstName<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor: Jean-francois Nepton<br>
+ *   Created: Mar 26, 2016
+ *
+ */
+
+/**
+ * Counting //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ *
+ * @author LastName, FirstName
+ * @version 1.0.0
+ * @since 1.0
+ *
+ */
+public class Counting {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// loop
+		for (int i = 0; i < 101; i++) {
+			System.out.println("Counting " + i);
+		}
+
+	}
+}
