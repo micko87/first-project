43256ad94d634f5e5ddda335817bf344a9e5ec1a
mikias ayalew <ammrdf@yahoo.com>
2016-03-26 18:11:39 -0700

    EvenNumber

---
+++ src/main/java/EvenNumber.java
@@ -0,0 +1,39 @@
+/**
+ *   File Name: EvenNumber.java<br>
+ *
+ *   LastName, FirstName<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor: Jean-francois Nepton<br>
+ *   Created: Mar 26, 2016
+ *
+ */
+
+/**
+ * EvenNumber //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ *
+ * @author LastName, FirstName
+ * @version 1.0.0
+ * @since 1.0
+ *
+ */
+public class EvenNumber {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// loop
+		for (int i = 0; i < 101; i++) {
+
+			if (i % 2 == 0) {
+
+				System.out.println("Counting " + i);
+			}
+		}
+	}
+
+}
